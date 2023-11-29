0a5f987ffb21bb33cf0a1329b823071745f390b9
Hugh Sorby <h.sorby@auckland.ac.nz>
2023-11-29 13:30:17 +1300

    Set location.

--- mapclientplugins/generateflatmapsourcesstep/__init__.py
+++ mapclientplugins/generateflatmapsourcesstep/__init__.py
@@ -6,7 +6,7 @@ MAP Client Plugin - Generated from MAP Client v0.20.0
 __version__ = '0.1.0'
 __author__ = 'Hugh Sorby'
 __stepname__ = 'Generate Flatmap Sources'
-__location__ = ''
+__location__ = 'https://github.com/mapclient-plugins/mapclientplugins.generateflatmapsourcesstep'
 
 # import class that derives itself from the step mountpoint.
 from mapclientplugins.generateflatmapsourcesstep import step
